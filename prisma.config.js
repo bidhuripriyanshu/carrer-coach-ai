5b1092e09e7070c16f1e59834c0a0c4fb79e6b43
Priyanshu bidhuri <156426411+bidhuripriyanshu@users.noreply.github.com>
2026-05-19 17:54:04 +0530

    changes3

--- prisma.config.js
+++ prisma.config.js
@@ -2,9 +2,6 @@ require("dotenv").config();
 const { defineConfig } = require("prisma/config");
 
 module.exports = defineConfig({
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
   migrate: {
     url: process.env.DATABASE_URL,
   },
